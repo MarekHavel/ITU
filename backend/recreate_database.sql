5bc680cb57e14dd57b3350f68966efff6262503b
Robin Volf <xvolfr00@stud.fit.vutbr.cz>
2023-11-05 10:20:12 +0100

    Update skriptu pro vytvoření databáze, podle diagramu, naplnění sample hodnotami

--- backend/recreate_database.sql
+++ backend/recreate_database.sql
@@ -1,24 +1,17 @@
 -- Autor: Robin Volf (xvolfr00)
 
--- Vyčištění databáze
+-- Vyčištění databáze (v opačném pořadí k vytvoření kvůli FK omezením)
 DROP TABLE IF EXISTS Orders;
 DROP TABLE IF EXISTS Menus;
 DROP TABLE IF EXISTS DishRatings;
-DROP TABLE IF EXISTS Users;
-DROP TABLE IF EXISTS Canteens;
+DROP TABLE IF EXISTS DishPrices;
 DROP TABLE IF EXISTS Dishes;
+DROP TABLE IF EXISTS Users;
+DROP TABLE IF EXISTS PriceCategories;
+DROP TABLE IF EXISTS DishCategories;
+DROP TABLE IF EXISTS Canteens;
 
 -- Vytvoření tabulek
-
-CREATE TABLE Users (
-    UserId integer AUTO_INCREMENT,
-    Name varchar(255) NOT NULL,
-    Email varchar(255) NOT NULL,
-    PasswordHash varchar(255) NOT NULL,
-    Credit integer,
-    -- Nějaké nastavení? nebo to lokálně?
-    PRIMARY KEY (UserId)
-);
 
 CREATE TABLE Canteens (
     CanteenId integer AUTO_INCREMENT,
@@ -30,15 +23,59 @@ CREATE TABLE Canteens (
     PRIMARY KEY (CanteenId)
 );
 
+CREATE TABLE DishCategories(
+    DishCategoryId integer AUTO_INCREMENT,
+    Name varchar(255) NOT NULL,
+    PRIMARY KEY (DishCategoryId)
+);
+
+CREATE TABLE PriceCategories(
+    PriceCategoryId integer AUTO_INCREMENT,
+    Name varchar(255) NOT NULL,
+    PRIMARY KEY (PriceCategoryId)
+);
+
+CREATE TABLE Users (
+    UserId integer AUTO_INCREMENT,
+    CanteenId integer,
+    PriceCategoryId integer,
+    Name varchar(255) NOT NULL,
+    Email varchar(255) NOT NULL,
+    PasswordHash varchar(255) NOT NULL,
+    Credit integer,
+    PRIMARY KEY (UserId),
+    FOREIGN KEY (CanteenId) REFERENCES Canteens(CanteenId),
+    FOREIGN KEY (PriceCategoryId) REFERENCES PriceCategories(PriceCategoryId)
+);
+
 CREATE TABLE Dishes (
     DishId integer AUTO_INCREMENT,
+    DishCategoryId integer,
     Name varchar(255) NOT NULL,
     Ingredients varchar(255) NOT NULL,
     Allergens varchar(255),
-    Type varchar(255) NOT NULL,
-    Price integer NOT NULL, -- Netuším formát
+    Mass integer NOT NULL,
     -- Image? Našel jsem info, že je lepší uložit jen referenci a mít to ve FS
-    PRIMARY KEY (DishId)
+    PRIMARY KEY (DishId),
+    FOREIGN KEY (DishCategoryId) REFERENCES DishCategories(DishCategoryId)
+);
+
+CREATE TABLE DishPrices (
+    DishId integer,
+    PriceCategoryId integer,
+    Price integer NOT NULL,
+    PRIMARY KEY (DishId, PriceCategoryId),
+    FOREIGN KEY (PriceCategoryId) REFERENCES PriceCategories(PriceCategoryId),
+    FOREIGN KEY (DishId) REFERENCES Dishes(DishId)
+);
+
+CREATE TABLE DishRatings (
+    UserId integer,
+    DishId integer,
+    Stars integer,
+    PRIMARY KEY (UserId, DishId),
+    FOREIGN KEY (DishId) REFERENCES Dishes(DishId),
+    FOREIGN KEY (UserId) REFERENCES Users(UserId)
 );
 
 CREATE TABLE Menus (
@@ -48,15 +85,6 @@ CREATE TABLE Menus (
     PRIMARY KEY (DishId, CanteenId, Date),
     FOREIGN KEY (DishId) REFERENCES Dishes(DishId),
     FOREIGN KEY (CanteenId) REFERENCES Canteens(CanteenId)
-);
-
-CREATE TABLE DishRatings (
-    UserId integer,
-    DishId integer,
-    Start integer,
-    PRIMARY KEY (UserId, DishId),
-    FOREIGN KEY (DishId) REFERENCES Dishes(DishId),
-    FOREIGN KEY (UserId) REFERENCES Users(UserId)
 );
 
 CREATE TABLE Orders(
@@ -71,24 +99,116 @@ CREATE TABLE Orders(
     FOREIGN KEY (DishId, CanteenId, Date) REFERENCES Menus(DishId, CanteenId, Date)
 );
 
-INSERT INTO Users (Name, Email, PasswordHash, Credit) -- Pro přihlášení novak345@gmail.com heslo
-VALUES ("Jan Novák", "novak345@gmail.com", "$2y$10$Lolv0oYdErm7gVEQGbgBCeq2yWVsORcoEgiupN24RVM0477fORzaG", 1000000);
+INSERT INTO Canteens(Name, Email, PhoneNumber, OpeningHours, Address) VALUES
+("Jídelna1", "jidelna1@jidelnybrno.com", "+420598927106", "idkxd", "Purkyňova 2640/93, 61200 Brno - Královo Pole, Česko"),
+("Jídelna2", "jidelna2@jidelnybrno.com", "+420599999999", "idktakyxd", "Purkyňova 2640/94, 61200 Brno - Královo Pole, Česko");
 
-INSERT INTO Users (Name, Email, PasswordHash, Credit) -- Pro přihlášení novotnyboi829@gmail.com 12345
-VALUES ("Josef Novotný", "novotnyboi829@gmail.com", "$2y$10$U1UFeaYQfxxrdIntnY2x6eWLsKMTJt8z.Oi/Fxipfqt7o5WA1QUky", 78);
+INSERT INTO DishCategories(Name) VALUES 
+("Hlavní jídla"),
+("Polévky"),
+("Přílohy");
 
-INSERT INTO Canteens(Name, Email, PhoneNumber, OpeningHours, Address)
-VALUES ("Jídelna1", "jidelna1@jidelnybrno.com", "+420598927106", "idkxd", "Purkyňova 2640/93, 61200 Brno - Královo Pole, Česko");
-INSERT INTO Canteens(Name, Email, PhoneNumber, OpeningHours, Address)
-VALUES ("Jídelna2", "jidelna2@jidelnybrno.com", "+420599999999", "idktakyxd", "Purkyňova 2640/94, 61200 Brno - Královo Pole, Česko");
+INSERT INTO PriceCategories(Name) VALUES
+("Student"),
+("Externista");
 
-INSERT INTO Dishes(Name, Ingredients, Allergens, Type, Price)
-VALUES ("Kuřecí plátek hodně na divoko", "Kuře, moře, stavení", "Alergen1, alergen2", "Hlavní jídlo", 68);
-INSERT INTO Dishes(Name, Ingredients, Type, Price)
-VALUES ("Kuřecí vývar, vlažný", "Kuře, voda, nudle", "Polévka", 20);
+INSERT INTO Users (CanteenId, PriceCategoryID, Name, Email, PasswordHash, Credit) VALUES 
+ -- Pro přihlášení novak345@gmail.com heslo
+(1, 1, "Jan Novák", "novak345@gmail.com", "$2y$10$Lolv0oYdErm7gVEQGbgBCeq2yWVsORcoEgiupN24RVM0477fORzaG", 1000000),
+ -- Pro přihlášení novotnyboi829@gmail.com 12345
+(2, 1, "Josef Novotný", "novotnyboi829@gmail.com", "$2y$10$U1UFeaYQfxxrdIntnY2x6eWLsKMTJt8z.Oi/Fxipfqt7o5WA1QUky", 78);
+
+-- Kategorie: 1 = hl. jídlo, 2 = polévka, 3 = příloha
+INSERT INTO Dishes(DishCategoryId, Name, Ingredients, Allergens, Mass) VALUES
+(1, "Kuřecí plátek na divoko", "Kuřecí maso, sůl, kmín, řepkový olej", "Lepek", 200),
+(1, "Plněný paprikový lusk", "Paprika, vepřové maso, kmín, cibule", "Lepek", 220),
+(1, "Svíčková na smetaně", "Vepřové maso, mrkev, petržel, smetana, celer", "Lepek, Vejce, Mléko, Hořčice", 350),
+(1, "Španělský ptáček", "Hovězí maso, slanina, salám, okurky, vejce, hladká mouka", "Lepek, Vejce, Hořčice", 250),
+(1, "Dukátové buchtičky s krémem", "Hladká mouka, mléko, cukr, žloutek, skořice", "Lepek, vejce, mléko", 270),
+(2, "Vývar s nudlemi", "Kuřecí vývar, nudle z pšeničné mouky", "Lepek, vejce", 150),
+(2, "Dršťková", "Dršťky, cibule, hladká mouka, česnek", "Lepek", 150),
+(3, "Rýže", "Jasmínová rýže", NULL, 100),
+(3, "Hranolky", "Brambory, olej, sůl", NULL, 100),
+(3, "Bramborová kaše", "Brambory, Mléko", "Mléko", 100),
+(3, "Knedlík", "Vejce, hladká mouka", "Vejce", 100);
+
+-- Vložení cen jídla pro kategorii "student"
+INSERT INTO DishPrices(DishId, PriceCategoryId, Price) VALUES
+(1, 1, 54),
+(2, 1, 61),
+(3, 1, 80),
+(4, 1, 57),
+(5, 1, 44),
+(6, 1, 20),
+(7, 1, 20),
+(8, 1, 15),
+(9, 1, 22),
+(10, 1, 18),
+(11, 1, 20);
 
 -- Vložení nějakých nabídek jídel pro jídelnu 1
-INSERT INTO Menus(DishId, CanteenId, Date)
-VALUES (1, 1, '2023-11-11');
-INSERT INTO Menus(DishId, CanteenId, Date)
-VALUES (2, 1, '2023-11-11');
+-- Nasimulovaný týden 13.11 - 17.11
+INSERT INTO Menus(DishId, CanteenId, Date) VALUES
+-- Pondělí --
+-- Jídelna 1
+(1, 1, '2023-11-13'),
+(2, 1, '2023-11-13'),
+(8, 1, '2023-11-13'),
+(10, 1, '2023-11-13'),
+(6, 1, '2023-11-13'),
+-- Jídelna 2
+(2, 2, '2023-11-13'),
+(3, 2, '2023-11-13'),
+(9, 2, '2023-11-13'),
+(11, 2, '2023-11-13'),
+(7, 2, '2023-11-13'),
+-- Úterý --
+-- Jídelna 1
+(2, 2, '2023-11-14'),
+(3, 2, '2023-11-14'),
+(9, 2, '2023-11-14'),
+(11, 2, '2023-11-14'),
+(7, 2, '2023-11-14'),
+-- Jídelna 2
+(1, 1, '2023-11-14'),
+(2, 1, '2023-11-14'),
+(8, 1, '2023-11-14'),
+(10, 1, '2023-11-14'),
+(6, 1, '2023-11-14'),
+-- Středa --
+-- Jídelna 1
+(4, 1, '2023-11-15'),
+(5, 1, '2023-11-15'),
+(9, 1, '2023-11-15'),
+(6, 1, '2023-11-15'),
+-- Jídelna 2
+(3, 2, '2023-11-15'),
+(4, 2, '2023-11-15'),
+(10, 2, '2023-11-15'),
+(7, 2, '2023-11-15'),
+-- Čtvrtek --
+-- Jídelna 1
+(3, 2, '2023-11-16'),
+(4, 2, '2023-11-16'),
+(10, 2, '2023-11-16'),
+(8, 2, '2023-11-16'),
+(7, 2, '2023-11-16'),
+-- Jídelna 2
+(4, 1, '2023-11-16'),
+(5, 1, '2023-11-16'),
+(9, 1, '2023-11-16'),
+(10, 1, '2023-11-16'),
+(6, 1, '2023-11-16'),
+-- Pátek --
+-- Jídelna 1
+(1, 1, '2023-11-17'),
+(2, 1, '2023-11-17'),
+(8, 1, '2023-11-17'),
+(10, 1, '2023-11-17'),
+(6, 1, '2023-11-17'),
+-- Jídelna 2
+(1, 2, '2023-11-17'),
+(2, 2, '2023-11-17'),
+(8, 2, '2023-11-17'),
+(10, 2, '2023-11-17'),
+(7, 1, '2023-11-17');
