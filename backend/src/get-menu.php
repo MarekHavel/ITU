54f1981fa0c6dbdcc8f5b0706a24a6bc3e0ef008
Robin Volf <xvolfr00@stud.fit.vutbr.cz>
2023-11-05 12:19:37 +0100

    Přidání endpointu pro získání menu

---
+++ backend/src/get-menu.php
@@ -0,0 +1,67 @@
+<?php
+
+include "common.php";
+
+if($_SERVER["REQUEST_METHOD"] != "POST") {
+    http_response_code(400);
+    returnError("Špatná metoda přístupu", 3);
+}
+
+// request body
+$json = file_get_contents("php://input");
+$request = json_decode($json, true);
+
+if(!array_key_exists("userId", $request)) {
+    returnError("Chybí parametr userId", 2);
+}
+
+$userId = $request["userId"];
+
+if(array_key_exists("date", $request)) {
+    $date = $request["date"];
+} else {
+    $date = date("Y-m-d");
+}
+
+$stmt = $pdo->prepare("SELECT CanteenId FROM Users WHERE UserId = ?");
+$stmt->execute(array($userId));
+
+if($stmt->rowCount() == 0) {
+    returnError("Nesprávné UserId", 1);
+}
+
+$canteenId = $stmt->fetchColumn();
+// var_dump($canteenId);
+
+$stmt = $pdo->prepare("SELECT DishId FROM Menus WHERE CanteenId = ? AND Date = ?");
+$stmt->execute(array($canteenId, $date));
+
+// 0 = DishId
+$dishIdArray = $stmt->fetchAll(PDO::FETCH_COLUMN, 0);  
+// var_dump($dishIdArray);
+
+$response = array();
+foreach($dishIdArray as $dishId){
+    // detaily jídla
+    $stmt = $pdo->prepare("SELECT * FROM Dishes WHERE DishId = ?");
+    $stmt->execute(array($dishId));
+    $dish_row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    // jméno DishCategory
+    $stmt = $pdo->prepare("SELECT Name FROM DishCategories WHERE DishCategoryId = ?");
+    $stmt->execute(array($dish_row["DishCategoryId"]));
+    $category_name = $stmt->fetch(PDO::FETCH_COLUMN, 0);
+    
+    $dish = array(
+        "id" => $dishId,
+        "name" => $dish_row["Name"],
+        "category" => $category_name,
+        "allergens" => $dish_row["Allergens"],
+        "itemsLeft" => 100, //TODO
+        
+    );
+    array_push($response, $dish);
+}
+
+echo json_encode($response, JSON_UNESCAPED_UNICODE);
+?>
