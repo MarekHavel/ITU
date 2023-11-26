d1afaf53e4a10788bd85f4c597f59dcc52f34f52
Marek Gergel <mage26gg@gmail.com>
2023-11-26 03:51:58 +0100

    Added settings fragment
    Fixed UI issues

---
+++ frontend/app/src/main/java/eu/havy/canteen/ui/settings/SettingsFragment.java
@@ -0,0 +1,48 @@
+package eu.havy.canteen.ui.settings;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceManager;
+
+import eu.havy.canteen.R;
+
+public class SettingsFragment extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener {
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.root_preferences, rootKey);
+        PreferenceManager.getDefaultSharedPreferences(requireContext()).registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, @Nullable String key) {
+        if (key == null) return;
+
+        switch (key) {
+            case "color_scheme":
+                String color_scheme = sharedPreferences.getString(key, "system_selected");
+                switch (color_scheme) {
+                    case "system_selected":
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
+                        break;
+                    case "dark_mode":
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                        break;
+                    case "light_mode":
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                        break;
+                }
+                break;
+            case "notifications":
+                boolean notifications = sharedPreferences.getBoolean(key, true);
+                if (notifications) {
+                    //api call to register for notifications
+                }
+                break;
+        }
+    }
+}
