552c25af330e148154c5aef22586604563686aff
Robin Volf <xvolfr00@stud.fit.vutbr.cz>
2023-11-28 12:47:17 +0100

    Redesign a implementace endpointu pro autentizaci

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var authController = require("../controllers/auth");
 
+// Autentizace
 router.post("/auth", authController.authenticate);
 
 // router.post("/menu", );
